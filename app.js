c5251b5e3e57214ca472504a301e4bc44e5bbcbb
basim341e <basimabid3@gmaile.com>
2022-12-08 18:23:42 -0800

    me

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const hostname = '127.0.0.1';
 const app = express()
 const path = require('path')
 const port = 80;
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/static', express.static(path.join(__dirname, 'views')))
 // app.set('view engine', 'pug') 
 app.set('views', path.join(__dirname, 'views'))
 app.set('views', './views')
-
-// app.use(express.static(`Views`))
 
  
 
@@ -22,8 +22,8 @@ app.get(`/aboutus`, (req, res) => {
   res.render(`aboutus.html`)
  })
 
-// app.listen(port, hostname, () => {
-  // console.log(`Server running at http://${hostname}:${port}/`);
-// }); 
-app.listen( process.env.PORT || 80, ()=> console.log(`server is running successfully `));
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+}); 
+// app.listen( process.env.PORT || 80, ()=> console.log(`server is running successfully `));
   
