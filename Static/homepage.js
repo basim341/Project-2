a97a5a39ec5e7c74f6c9e641261a654809ce0f89
basim341e <basimabid3@gmaile.com>
2023-01-05 00:42:38 +0500

    up

--- Static/homepage.js
+++ Static/homepage.js
@@ -1,16 +1,7 @@
 // alert(`Note: This is an imaginary website and any stated facts or figures are not true`)
 
-
- let sidebarbtn =    ()  => {   
- let btn = document.getElementById(`m-container`).style
- if(btn.display != `flex`){
- btn.display =`flex`
- btn.transition = `0.5s all ease`
- }
- else{
-    btn.display = `none`
- }
- }
+    const btn = document.querySelector(`.btn`)
+    const sidebar = document.querySelector(`.sidbar`)
 
  const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
