a146b83441eedd8d0c39e5295ee5d187a215a7d0
basim341e <basimabid3@gmaile.com>
2023-01-04 09:29:43 +0500

    gemlo

--- Static/homepage.js
+++ Static/homepage.js
@@ -1,1 +1,19 @@
 // alert(`Note: This is an imaginary website and any stated facts or figures are not true`)
+
+
+ let sidebarbtn =    ()  => {   
+ let btn = document.getElementById(`m-container`).style
+ if(btn.display != `flex`){
+ btn.display =`flex`
+ btn.transition = `0.5s all ease`
+ }
+ else{
+    btn.display = `none`
+ }
+
+ 
+ 
+ 
+
+ 
+ }
