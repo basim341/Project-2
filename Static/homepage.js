29ca010fa3c4669d24b3651cac6e940ba6f46d26
basim341e <basimabid3@gmaile.com>
2023-01-04 10:26:04 +0500

    latest

--- Static/homepage.js
+++ Static/homepage.js
@@ -10,10 +10,18 @@
  else{
     btn.display = `none`
  }
-
- 
- 
- 
-
- 
  }
+
+ const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) [
+            entry.target.classList.add(`show`)
+        ]
+        else {
+            entry.target.classList.remove(`show`)
+        }
+    })
+ })
+
+ const hiddenelements = document.querySelectorAll(`.hidden`) 
+ hiddenelements.forEach((el) => observer.observe(el));
