5d9da3629e8966b140d0bd6774d5c4160b70f604
basim341e <basimabid3@gmaile.com>
2023-01-05 08:27:02 +0500

    chck

--- Static/homepage.js
+++ Static/homepage.js
@@ -1,7 +1,14 @@
 // alert(`Note: This is an imaginary website and any stated facts or figures are not true`)
 
     const btn = document.querySelector(`.btn`)
-    const sidebar = document.querySelector(`.sidbar`)
+    const sidebar = document.querySelector(`.sidebar`)
+    btn.addEventListener(`click`, () => {
+        btn.classList.toggle(`active`)
+        sidebar.classList.toggle(`active`)
+        
+    }
+    
+    )
 
  const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
